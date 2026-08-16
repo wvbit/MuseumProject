fd507f11dc60492574b06f3a6c11a6aa98d88238
wvbit <roantaru@gmail.com>
2026-08-16 12:05:15 +0300

    new update

---
+++ Assets/Hash Game studios/Herbal & Medic Pack/Scripts/Core/GameConfig/Globals.cs
@@ -0,0 +1,11 @@
+namespace HashGame.HearbMedicPack
+{
+    public class Globals
+    {
+        public const string PublisherName= "Hash Game studios";
+        public const string ProjectName= "Herbal & Medic Pack";
+        public const string PublisherPage = "https://assetstore.unity.com/publishers/113438";
+        public const string SupportEmail = "hashgame.cs@gmail.com";
+        public const string YouTube = "https://www.youtube.com/@HashGameStudio";
+    }
+}
